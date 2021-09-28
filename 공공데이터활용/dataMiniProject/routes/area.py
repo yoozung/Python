d47ca74084a2ef444333a9539e0b0423e31916cc
uurj <dbwjd1154@gmail.com>
2021-09-29 00:25:22 +0900

    [ Python ]

--- 공공데이터활용/dataMiniProject/routes/area.py
+++ 공공데이터활용/dataMiniProject/routes/area.py
@@ -10,18 +10,3 @@ def regionRank():
     y = request.form['y']
     rank:list = service.regionRank(y=y)
     return render_template('rank/regionRank.html', rank=rank, y=y)
-
-'''
-@bp.route('/route-info', methods=['POST'])
-def route_info():
-    routeid = request.form['routeid']
-    bus:mo.Bus = service.getRouteInfo(routeId=routeid)
-    return render_template('bus/businfo.html', bus=bus)
-
-
-@bp.route('/regionRank', methods=['POST'])
-def regionRank():
-    areaNm = request.form['areaNm']
-    rank = service.regionRank(areaNm=areaNm)
-    return render_template('rank/regionRank.html', rank=rank)
-'''
